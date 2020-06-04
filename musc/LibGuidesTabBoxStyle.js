7000b454633d70b796545f338789a02ca006070f
dwtaber <38249738+dwtaber@users.noreply.github.com>
2020-06-04 11:05:23 -0400

    Create LibGuidesTabBoxStyle.js

---
+++ musc/LibGuidesTabBoxStyle.js
@@ -0,0 +1,29 @@
+// ==UserScript==
+// @name         LibGuides Tab Box Style
+// @namespace    https://github.com/dwtaber/userscripts
+// @version      0.1
+// @updateURL    https://raw.githubusercontent.com/dwtaber/Userscripts/musc/LibGuidesTabBoxStyle.js
+// @downloadURL  https://raw.githubusercontent.com/dwtaber/Userscripts/musc/LibGuidesTabBoxStyle.js
+// @description  Allows volume adjustment for LibChat notification sounds
+// @author       dwtaber@gmail.com
+// @match        http://musc.libguides.com/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    //Inject style into document head.
+    const volumeSliderStyle = `
+    ul.nav.nav-tabs li.active a {
+        color: #005486
+    }
+
+    ul.nav.nav-tabs a {
+        color: #999;
+        font-size: 0.9em !important;
+        font-weight: 600;
+    }
+    `;
+    
+})();
