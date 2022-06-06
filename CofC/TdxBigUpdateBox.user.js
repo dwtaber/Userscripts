b682848f3490af7cce412c3d16269a7c4fbd24f0
dwtaber <dwtaber@gmail.com>
2022-06-06 19:37:26 -0400

    Fixed match condition

--- CofC/TdxBigUpdateBox.user.js
+++ CofC/TdxBigUpdateBox.user.js
@@ -6,7 +6,7 @@
 // @downloadURL  https://raw.githubusercontent.com/dwtaber/Userscripts/master/CofC/TdxBigUpdateBox.user.js
 // @description  Taller default comment box in ticket update UI
 // @author       Dan Taber
-// @match        https://cofc.teamdynamix.com/TDNext/Apps/362/Tickets/Update
+// @match        https://cofc.teamdynamix.com/TDNext/Apps/362/Tickets/Update*
 // @grant        none
 // ==/UserScript==
 
