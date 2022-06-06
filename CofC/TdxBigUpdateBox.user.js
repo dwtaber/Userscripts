417dbafe7d15f58c53b51e063916af1065b1b87a
dwtaber <dwtaber@gmail.com>
2022-06-06 19:38:45 -0400

    Incremented version TdxBigUpdateBox

--- CofC/TdxBigUpdateBox.user.js
+++ CofC/TdxBigUpdateBox.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Tdx Big Update Box
 // @namespace    https://github.com/dwtaber/Userscripts
-// @version      0.1
+// @version      0.1.1
 // @updateURL    https://raw.githubusercontent.com/dwtaber/Userscripts/master/CofC/TdxBigUpdateBox.user.js
 // @downloadURL  https://raw.githubusercontent.com/dwtaber/Userscripts/master/CofC/TdxBigUpdateBox.user.js
 // @description  Taller default comment box in ticket update UI
