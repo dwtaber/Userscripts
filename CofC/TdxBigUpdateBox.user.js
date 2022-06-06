0fe98a07a96d3e8a318e96d4cdf11598974f71db
dwtaber <dwtaber@gmail.com>
2022-06-06 19:35:53 -0400

    Added TdxBigUpdateBox, updated TdxAutoRefresh URI

---
+++ CofC/TdxBigUpdateBox.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         Tdx Big Update Box
+// @namespace    https://github.com/dwtaber/Userscripts
+// @version      0.1
+// @updateURL    https://raw.githubusercontent.com/dwtaber/Userscripts/master/CofC/TdxBigUpdateBox.user.js
+// @downloadURL  https://raw.githubusercontent.com/dwtaber/Userscripts/master/CofC/TdxBigUpdateBox.user.js
+// @description  Taller default comment box in ticket update UI
+// @author       Dan Taber
+// @match        https://cofc.teamdynamix.com/TDNext/Apps/362/Tickets/Update
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    document.querySelector("#divComments textarea").style.height = "600px";
+})();
