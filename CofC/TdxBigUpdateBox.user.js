1f7d0b7263dac2a21f0aa898b43e5c26531ef192
dwtaber <dwtaber@gmail.com>
2022-06-07 17:03:52 -0400

    Changed match criteria for BigUpdateBox

--- CofC/TdxBigUpdateBox.user.js
+++ CofC/TdxBigUpdateBox.user.js
@@ -1,17 +1,19 @@
 // ==UserScript==
 // @name         Tdx Big Update Box
 // @namespace    https://github.com/dwtaber/Userscripts
-// @version      0.1.1
+// @version      0.1.2
 // @updateURL    https://raw.githubusercontent.com/dwtaber/Userscripts/master/CofC/TdxBigUpdateBox.user.js
 // @downloadURL  https://raw.githubusercontent.com/dwtaber/Userscripts/master/CofC/TdxBigUpdateBox.user.js
 // @description  Taller default comment box in ticket update UI
 // @author       Dan Taber
-// @match        https://cofc.teamdynamix.com/TDNext/Apps/362/Tickets/Update*
+// @match        https://*.teamdynamix.com/TDNext/Apps/*/Tickets/Update*
 // @grant        none
 // ==/UserScript==
 
 (function() {
     'use strict';
 
-    document.querySelector("#divComments textarea").style.height = "600px";
+    let boxHeight = "600px";
+
+    document.querySelector("#divComments textarea").style.height = boxHeight;
 })();
