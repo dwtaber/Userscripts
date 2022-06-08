afeba1e2e0047076f2d518702b79b57af13fbdc6
dwtaber <dwtaber@gmail.com>
2022-06-07 20:50:42 -0400

    Made TdxAccountShortcuts easier to customize.

--- CofC/TdxAccountShortcuts.user.js
+++ CofC/TdxAccountShortcuts.user.js
@@ -1,17 +1,28 @@
 // ==UserScript==
 // @name         TDX Account Shortcuts
-// @version      2021.06.22.1
+// @version      2022.6.7.0
 // @downloadURL  https://raw.githubusercontent.com/dwtaber/Userscripts/master/CofC/TdxAccountShortcuts.user.js
 // @updateURL    https://raw.githubusercontent.com/dwtaber/Userscripts/master/CofC/TdxAccountShortcuts.user.js
 // @namespace    https://github.com/dwtaber/Userscripts
 // @supportURL   https://github.com/dwtaber/Userscripts
 // @description  Creates shortcut buttons for selecting common account/department values for new users.
 // @author       Dan Taber
-// @match        https://cofc.teamdynamix.com/TDNext/Apps/People/PersonNew
-// @match        https://cofc.teamdynamix.com/SBTDNext/Apps/People/PersonNew
-// @match        https://cofc.teamdynamix.com/TDNext/Apps/People/PersonEdit.aspx*
+// @match        https://*.teamdynamix.com/TDNext/Apps/People/PersonNew
+// @match        https://*.teamdynamix.com/SBTDNext/Apps/People/PersonNew
+// @match        https://*.teamdynamix.com/TDNext/Apps/People/PersonEdit.aspx*
 // @grant        none
 // ==/UserScript==
+
+
+// Organization field will prefill with this name.
+let orgName = "College of Charleston";
+
+// Account name and ID number of each account we want a button for.
+let accounts = [
+    {name: "Student", id: 45430},
+    {name: "Parent", id: 59055},
+    {name: "Alumni", id: 45431}
+];
 
 // Takes the TDX account name and ID provided and produces the node for
 // a button that selects that account without going through the search.
@@ -23,24 +34,10 @@ function makeQuickButton(accountName,accountID)
     button.accountID = accountID;
     button.type = "button";
     button.className = "quickButton";
-    button.id = `quickButton-${accountName}`;
+    button.id = `quickButton-${accountID}`;
     button.onclick = function(){acb_taluDepartment.addItem(accountName,accountID);}
     return button;
 }
-
-// Not sure any styling will be necessary.
-let quickButtonStyle = document.createElement("style");
-quickButtonStyle.innerHTML = `
-    button.quickButton
-    {
-    }
-
-    div.quickButtonContainer
-    {
-
-    }
-`;
-
 
 // Create a div to contain the buttons we create.
 let buttonContainer = document.createElement("div");
@@ -48,18 +45,10 @@ buttonContainer.id = "quickButtonContainer";
 buttonContainer.className = "quickButtonContainer";
 
 // Create the specific buttons we want.
-let studentButton = makeQuickButton("Student",45430);
-let parentButton = makeQuickButton("Parent",59055);
-let alumniButton = makeQuickButton("Alumni",45431);
-
-buttonContainer.append(studentButton,parentButton,alumniButton);
-
+accounts.forEach(account => buttonContainer.append(makeQuickButton(account.name,account.id)));
 
 // Prefill the Organization field.
-document.getElementById("txtOrganization").value = "College of Charleston";
-
-// Inject the style sheet into the head tag.
-document.head.appendChild(quickButtonStyle);
+document.getElementById("txtOrganization").value = orgName;
 
 // Inject the buttons and their container between the label and the input box.
 document.getElementById("divAccount")
