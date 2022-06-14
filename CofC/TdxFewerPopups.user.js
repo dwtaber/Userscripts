06dee0370902f84b2797cc7423b6b02061b4d043
dwtaber <dwtaber@gmail.com>
2022-06-14 19:21:02 -0400

    Added handling for ticket updates; configurability

--- CofC/TdxFewerPopups.user.js
+++ CofC/TdxFewerPopups.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         TDX Fewer Popups
-// @version      0.0.1
+// @version      0.0.3
 // @downloadURL  https://raw.githubusercontent.com/dwtaber/Userscripts/master/CofC/TdxFewerPopups.user.js
 // @updateURL    https://raw.githubusercontent.com/dwtaber/Userscripts/master/CofC/TdxFewerPopups.user.js
 // @namespace    https://github.com/dwtaber/Userscripts
@@ -12,14 +12,47 @@
 // @grant        none
 // ==/UserScript==
 
-function openWinToOpen (onclickNode)
+
+/* Easily configurable bits start here! */
+
+// Valid targets are:
+// _self    - The current browsing context (e.g. same frame if within a frameset)
+// _blank   - New tab or new window, depending on browser settings
+// _parent  - Parent of the current context; same as _self if no parent
+// _top     - Topmost ancestor of current context; same as _self if no ancestors
+const defaultTarget = '_top';
+
+// Same valid targets as above.
+// Note: after saving an update, if target is not _blank there's no in-page way to navigate back to the ticket detail page.
+// When set to _blank, the update page opens in a new tab (or window, depending on browser settings),
+// which closes automatically upon saving the update.
+const updateTarget = "_blank";
+
+/* End of easily configurable bits! */
+
+
+const onclickName = 'onclick';
+const targetName = 'target';
+
+function removeOpenWinHref (onclickNode, newTarget)
 {
-    let uri = onclickNode.getAttribute('onclick').split("'")[1]
-    onclickNode.setAttribute('onclick',`window.open('${uri}','_self')`)
+    onclickNode.removeAttribute(onclickName);
+    onclickNode.setAttribute(targetName, newTarget);
 }
 
+function openWinToOpen (onclickNode, newTarget)
+{
+    let uri = onclickNode.getAttribute(onclickName).split("'")[1];
+    onclickNode.setAttribute(onclickName,`window.open('${uri}','${newTarget}')`);
+}
+
+// After saving an update, there's no in-page way to navigate back to the ticket detail page.
+// This is a workaround until I figure out something cleaner.
+let updateLink = document.querySelector('#divUpdateFromActions')
+if (updateLink != null) {openWinToOpen(updateLink, updateTarget)}
+
 // Strip openWinHref function from links, since they seem to have a usable href attribute to fall back to.
-document.querySelectorAll('a[onclick*=openWinHref]').forEach( x => x.removeAttribute('onclick') );
+document.querySelectorAll('a[onclick*=openWinHref]').forEach( x => removeOpenWinHref(x, defaultTarget) );
 
 // Replace openWin function with window.open for buttons and such.
-document.querySelectorAll('[onclick*=openWin]').forEach( x => openWinToOpen(x) );
+document.querySelectorAll('[onclick*=openWin]').forEach( x => openWinToOpen(x, defaultTarget) );
