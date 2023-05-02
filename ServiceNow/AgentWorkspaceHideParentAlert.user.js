7e159e7eaa8190cfb12e9aa7d2a1f9f3b7c85696
dwtaber <dwtaber@gmail.com>
2023-05-02 18:55:31 -0400

    Updated HideParentAlert

--- ServiceNow/AgentWorkspaceHideParentAlert.user.js
+++ ServiceNow/AgentWorkspaceHideParentAlert.user.js
@@ -4,10 +4,10 @@
 // @downloadURL  https://github.com/dwtaber/Userscripts/raw/master/ServiceNow/AgentWorkspaceHideParentAlert.user.js
 // @updateURL    https://github.com/dwtaber/Userscripts/raw/master/ServiceNow/AgentWorkspaceHideParentAlert.user.js
 // @supportURL   https://github.com/dwtaber/Userscripts/issues
-// @version      0.1
+// @version      0.2
 // @description  Hides the "this is the primary ticket for..." notification on incidents
 // @author       Dan Taber (dwtaber@gmail.com)
-// @match        https://*.service-now.com/now/workspace/agent/record*
+// @match        https://*.service-now.com/now/workspace/agent/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=service-now.com
 // @grant        none
 // ==/UserScript==
@@ -25,9 +25,16 @@ function hideAlert() {
                                     .shadowRoot.querySelector("sn-form-internal-alert-list")
                                     .shadowRoot.querySelector("now-alert")
                                     .shadowRoot.querySelector(".now-alert-content.is-expanded div");
-    let notificationOuter = notificationInner.parentElement.parentElement.parentElement.parentElement;
-    if (notificationInner.innerText.match("This is the primary ticket for")) {
-        notificationOuter.style.display = 'none';
+    if (notificationInner != null)
+    {
+        try
+        {
+            let notificationOuter = notificationInner.parentElement.parentElement.parentElement.parentElement;
+            if (notificationInner.innerText.match("This is the primary ticket for")) {
+                notificationOuter.style.display = 'none';
+            }
+        }
+        catch {}
     }
 }
 
